52c286303d72c1efc00621b07c5c4c399d7d3803
JyothsnaRatnakaram <r.jyothsnasree@gmail.com>
2025-11-17 22:13:10 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,1 +1,3 @@
-document.getElementById('projects').innerHTML = '<p>Project 1</p>';
+document.getElementById('year')&&(document.getElementById('year').innerText=new Date().getFullYear());
+document.getElementById('year2')&&(document.getElementById('year2').innerText=new Date().getFullYear());
+document.getElementById('year3')&&(document.getElementById('year3').innerText=new Date().getFullYear());
